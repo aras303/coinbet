b87598577ab7d3aba22e19f11c0dd0ac6e2a2646
Mehmet Kara <parantez303@gmail.com>
2026-06-23 23:09:49 +0100

    all matches

--- api/matches.js
+++ api/matches.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
 
   const response = await fetch(
-    "https://api.football-data.org/v4/matches?status=SCHEDULED",
-    {
-      headers: {
-        "X-Auth-Token": "5abad29ba46d4c0082d41217d74e913d"
-      }
+  "https://api.football-data.org/v4/matches",
+  {
+    headers: {
+      "X-Auth-Token": "5abad29ba46d4c0082d41217d74e913d"
     }
-  );
+  }
+);
 
   const data = await response.json();
 
