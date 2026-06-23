03476baf6f9709be0af701c96f2c4b8ffa7a437d
Mehmet Kara <parantez303@gmail.com>
2026-06-23 22:52:38 +0100

    live matches

--- api/matches.js
+++ api/matches.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const response = await fetch(
-    "https://api.football-data.org/v4/matches",
+   "https://api.football-data.org/v4/matches?status=LIVE",
     {
       headers: {
         "X-Auth-Token": "5abad29ba46d4c0082d41217d74e913d"
