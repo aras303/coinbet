e2bdcb2abe63be90df36471115d0ae4db31175f0
Mehmet Kara <parantez303@gmail.com>
2026-06-24 01:32:27 +0100

    fix free api

--- api/matches.js
+++ api/matches.js
@@ -1,7 +1,9 @@
 export default async function handler(req, res) {
 
+  const today = new Date().toISOString().split("T")[0];
+
   const response = await fetch(
-    "https://v3.football.api-sports.io/fixtures?next=50",
+    `https://v3.football.api-sports.io/fixtures?date=${today}`,
     {
       headers: {
         "x-apisports-key": "dfd781a849a72523f6bcaa1ffc7c967a"
